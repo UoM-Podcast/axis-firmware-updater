c949f96cec6e73729221b22dadb32809a37a3a28
Andrew Wilson <andrew.wilson-2@manchester.ac.uk>
2018-12-10 15:25:28 +0000

    modifications to run standalone with a simple config file

--- axisFirmwareUpdater.py
+++ axisFirmwareUpdater.py
@@ -1,30 +1,59 @@
 import requests
 from requests.auth import HTTPDigestAuth
 import hashlib
+import configparser
 
-cam_address = ''
-cam_username = ''
-cam_password = ''
-axis_firmware_bin = 'V5915_5_75_1_11.bin'
-axis_firmware_hash = '82e44e68b580626b568b07a1b7258b445ab8e22ff4f18ddc167cf87588f0ec46'
+config = configparser.ConfigParser()
+config.read('config.ini')
+conf = config['DEFAULT']
+
+cam_username = conf['cam_username']
+cam_password = conf['cam_password']
+cam_list = conf['cam_list']
+axis_firmware_bin = 'firmware/' + conf['axis_firmware_bin']
+axis_firmware_hash = conf['axis_firmware_hash']
 
 
 def get_file_hash():
     filename = axis_firmware_bin
     sha256_hash = hashlib.sha256()
-    with open(filename,"rb") as f:
-        # Read and update hash string value in blocks of 4K
-        for byte_block in iter(lambda: f.read(4096),b""):
-            sha256_hash.update(byte_block)
-        return sha256_hash.hexdigest()
+    try:
+        with open(filename,"rb") as f:
+            # Read and update hash string value in blocks of 4K
+            for byte_block in iter(lambda: f.read(4096),b""):
+                sha256_hash.update(byte_block)
+            return sha256_hash.hexdigest()
+    except IOError as fileerror:
+        print('firmware file not found: ERROR! ' + str(fileerror))
 
 
-if axis_firmware_hash == get_file_hash():
-    print("yay!")
+def check_hash():
+    if axis_firmware_hash == get_file_hash():
+        return True
+    else:
+        return False
 
-headers = {'X-Requested-Auth': 'Digest'}
-files = {'file': open(axis_firmware_bin, 'rb')}
-response = requests.post('http://{}/axis-cgi/firmwareupgrade.cgi'.format(cam_address), headers=headers, auth=HTTPDigestAuth(cam_username, cam_password), files=files)
-print(response.status_code)
-print(response.content)
 
+def run_update(cam_address):
+    print('uploading firmware to the camera, please wait')
+    headers = {'X-Requested-Auth': 'Digest'}
+    try:
+        files = {'file': open(axis_firmware_bin, 'rb')}
+    except IOError as fileerror:
+        print('firmware file not found: ERROR! ' + str(fileerror))
+
+    try:
+        response = requests.post('http://{}/axis-cgi/firmwareupgrade.cgi'.format(cam_address), headers=headers, auth=HTTPDigestAuth(cam_username, cam_password), files=files)
+    except Exception as e:
+        print('network error communication with the camera: ERROR ' + str(e))
+        return
+    print('Status code: ' + response.status_code)
+    print(response.content)
+
+
+for i in cam_list.split():
+    if check_hash():
+        print('firmware file matches given checksum: OK!')
+        run_update(i)
+    else:
+        print('firmware file possibly corrupted, please re-download firmware: ERROR!')
